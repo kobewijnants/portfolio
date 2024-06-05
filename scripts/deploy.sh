cdda41c179e7ba9d4e6ee24be4fee390bb2c82db
Kobe Wijnants <kobe.wijnants@student.kdg.be>
2024-06-05 22:09:37 +0200

    fix script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,118 +1,137 @@
 #!/usr/bin/env bash
 
-print_red() {
-    echo -e "\033[0;31m$1\033[0m"
+# ANSI color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+END='\033[0m'
+
+print_colored() {
+    echo -e "${1}${2}${END}"
 }
 
-print_green() {
-    echo -e "\033[0;32m$1\033[0m"
-}
-
-print_yellow() {
-    echo -e "\033[0;33m$1\033[0m"
-}
-
-print_blue() {
-    echo -e "\033[0;34m$1\033[0m"
-}
-
-# Check to run as root
-if [[ $EUID -ne 0 ]]; then
-    print_red "This script must be run as root"
-    exit 1
-fi
-
-distro=$(awk -F= '/^NAME/{print $2}' /etc/os-release | awk '{print tolower($0)}')
-print_blue $distro
-
-# Check if apache is installed, if not do it
-if command -v apache2 &>/dev/null; then
-    print_blue "Apache already installed"
-elif command -v httpd &>/dev/null; then
-    print_blue "Apache already installed"
-else
-    case $distro in
-    *arch* | *manjaro*)
-        print_green "Arch based OS detected"
-        pacman -Sy apache curl
-        ;;
-    *debian* | *ubuntu* | *pop* | *mint*)
-        print_green "Debian based OS detected"
-        apt-get update -y
-        apt-get install apache2 curl -y
-        ;;
-    *fedora*)
-        print_green "Fedora detected"
-        dnf install httpd curl
-        ;;
-    *centos*)
-        if [[ $(awk -F= '/^VERSION_ID/{print $2}' /etc/os-release) == '"8"' ]]; then
-            print_green "CentOS 8 based OS detected"
-            dnf install httpd curl
-        else
-            print_green "CentOS 7 based OS detected"
-            yum install httpd curl
-        fi
-        ;;
-    *redhat* | *rhel*)
-        print_green "Red Hat detected"
-        yum install httpd curl
-        ;;
-    *)
-        echo "OS not detected, try to install apache manually."
+check_root() {
+    if [[ $EUID -ne 0 ]]; then
+        print_colored "${RED}This script must be run as root${END}"
         exit 1
-        ;;
+    fi
+}
+
+get_distro() {
+    distro=$(awk -F= '/^NAME/{print $2}' /etc/os-release | awk '{print tolower($0)}')
+    print_colored "${BLUE}$distro${END}"
+}
+
+check_apache() {
+    if command -v apache2 &>/dev/null; then
+        print_colored "${GREEN}Apache already installed${END}"
+    elif command -v httpd &>/dev/null; then
+        print_colored "${GREEN}Apache already installed${END}"
+    else
+        case $distro in
+            *arch* | *manjaro*)
+                print_colored "${GREEN}Arch based OS detected${END}"
+                pacman -Sy apache curl
+                ;;
+            *debian* | *ubuntu* | *pop* | *mint*)
+                print_colored "${GREEN}Debian based OS detected${END}"
+                apt-get update -y
+                apt-get install apache2 curl -y
+                ;;
+            *fedora*)
+                print_colored "${GREEN}Fedora detected${END}"
+                dnf install httpd curl
+                ;;
+            *centos*)
+                if [[ $(awk -F= '/^VERSION_ID/{print $2}' /etc/os-release) == '"8"' ]]; then
+                    print_colored "${GREEN}CentOS 8 based OS detected${END}"
+                    dnf install httpd curl
+                else
+                    print_colored "${GREEN}CentOS 7 based OS detected${END}"
+                    yum install httpd curl
+                fi
+                ;;
+            *redhat* | *rhel*)
+                print_colored "${GREEN}Red Hat detected${END}"
+                yum install httpd curl
+                ;;
+            *)
+                echo "OS not detected, try to install apache manually."
+                exit 1
+                ;;
+        esac
+    fi
+}
+
+install_nvm() {
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+}
+
+go_to_project_root() {
+    cd "$(git rev-parse --show-toplevel)"
+}
+
+pull_latest_changes() {
+    if ! git pull; then
+        print_colored "${RED}Error pulling latest changes from git${END}"
+        exit 1
+    fi
+}
+
+install_npm_packages() {
+    npm install
+}
+
+build_project() {
+    npm run build
+}
+
+find_apache_root_dir() {
+    case $distro in
+        *arch* | *manjaro*)
+            apache_root_dir="/srv/http"
+            ;;
+        *debian* | *ubuntu* | *pop* | *mint*)
+            apache_root_dir="/var/www/html"
+            ;;
+        *fedora*)
+            apache_root_dir="/var/www/html"
+            ;;
+        *centos*)
+            apache_root_dir="/var/www/html"
+            ;;
+        *redhat* | *rhel*)
+            apache_root_dir="/var/www/html"
+            ;;
+        *)
+            print_colored "${RED}OS not detected, try to find the apache root directory manually.${END}"
+            exit 1
+            ;;
     esac
-fi
+}
 
-# Install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-source ~/.bashrc
+copy_build_to_apache() {
+    rm -rf "$apache_root_dir"/*
+    cp -r dist/* "$apache_root_dir"
+}
 
-# Go to root of project
-cd "$(git rev-parse --show-toplevel)"
+restart_apache() {
+    if command -v apache2 &>/dev/null; then
+        systemctl restart apache2
+    elif command -v httpd &>/dev/null; then
+        systemctl restart httpd
+    fi
+}
 
-# Pull latest changes from git
-git pull
-
-
-
-# Install npm packages
-npm install
-
-# Build the project
-npm run build
-
-# Find the apache root directory
-case $distro in
-*arch* | *manjaro*)
-    apache_root_dir="/srv/http"
-    ;;
-*debian* | *ubuntu* | *pop* | *mint*)
-    apache_root_dir="/var/www/html"
-    ;;
-*fedora*)
-    apache_root_dir="/var/www/html"
-    ;;
-*centos*)
-    apache_root_dir="/var/www/html"
-    ;;
-*redhat* | *rhel*)
-    apache_root_dir="/var/www/html"
-    ;;
-*)
-    print_red "OS not detected, try to find the apache root directory manually."
-    exit 1
-    ;;
-esac
-
-# Copy the build to apache
-rm -rf $apache_root_dir/*
-cp -r dist/* $apache_root_dir
-
-# Restart apache
-if command -v apache2 &>/dev/null; then
-    systemctl restart apache2
-elif command -v httpd &>/dev/null; then
-    systemctl restart httpd
-fi
+# Main script
+check_root
+get_distro
+check_apache
+install_nvm
+go_to_project_root
+pull_latest_changes
+install_npm_packages
+build_project
+find_apache_root_dir
+copy_build_to_apache
+restart_apache
